f82cbf829fe60df3615e4adba6419700eb71948b
rohiimahn-beep <rohiimahn@gmail.com>
2026-06-11 11:04:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return """
-    <h1>🎓 CertiSign Academic</h1>
-    <p>Sistem Sertifikat Digital Mahasiswa</p>
-    """
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
